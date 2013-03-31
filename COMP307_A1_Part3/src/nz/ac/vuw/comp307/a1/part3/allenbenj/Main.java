5774f30a6f3a5ef123004c6a6688ac7cb569cc9d
Ben Allen <benos.9k@gmail.com>
2013-03-31 14:18:31 +1300

    Main classes added for parts 3 and 4.

---
+++ COMP307_A1_Part3/src/nz/ac/vuw/comp307/a1/part3/allenbenj/Main.java
@@ -0,0 +1,9 @@
+package nz.ac.vuw.comp307.a1.part3.allenbenj;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ COMP307_A1_Part4/src/nz/ac/vuw/comp307/a1/part4/allenbenj/Main.java
@@ -0,0 +1,9 @@
+package nz.ac.vuw.comp307.a1.part4.allenbenj;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
